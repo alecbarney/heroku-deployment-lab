a54de20a1aaf48dfa648e36cda60e071abf7234d
Alec Barney <alecbarney34@gmail.com>
2022-02-23 14:11:28 -0700

    css test

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express()
 app.get('/', (req, res)=>{
 res.sendFile(path.join(__dirname, '../index.html'))
 });
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.css'))
   })
